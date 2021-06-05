085333ef04415b13723d8597f2ea42156f12396c
fpemud <fpemud@sina.com>
2021-06-05 10:03:11 +0800

    fix

--- python3/robust_layer/_util.py
+++ python3/robust_layer/_util.py
@@ -29,7 +29,6 @@ import socket
 import shutil
 import selectors
 import subprocess
-import ptyprocess
 from . import TIMEOUT
 
 
@@ -129,112 +128,34 @@ class Util:
         Util._communicate(proc)
 
     @staticmethod
-    def cmdListPtyExec(cmdList, envDict=None):
-        proc = ptyprocess.PtyProcessUnicode.spawn(cmdList, env=envDict)
-        Util._communicateWithPty(proc)
-
-    @staticmethod
-    def cmdListPtyExecWithStuckCheck(cmdList, envDict={}, bQuiet=False):
-        proc = ptyprocess.PtyProcessUnicode.spawn(cmdList, env=envDict)
-        Util._communicateWithPtyStuckCheck(proc, bQuiet)
-
-    @staticmethod
-    def shellPtyExec(cmd, envDict=None):
-        proc = ptyprocess.PtyProcessUnicode.spawn(["/bin/sh", "-c", cmd], env=envDict)
-        Util._communicateWithPty(proc)
-
-    @staticmethod
-    def shellPtyExecWithStuckCheck(cmd, envDict=None, bQuiet=False):
-        proc = ptyprocess.PtyProcessUnicode.spawn(["/bin/sh", "-c", cmd], env=envDict)
-        Util._communicateWithPtyStuckCheck(proc, bQuiet)
-
-    @staticmethod
     def _communicate(proc):
-        # from nonblock_read() in https://github.com/kata198/python-nonblock/blob/HEAD/nonblock/read.py
-        # python sucks at non-block I/O
+        if hasattr(selectors, 'PollSelector'):
+            pselector = selectors.PollSelector
+        else:
+            pselector = selectors.SelectSelector
+
+        # redirect proc.stdout/proc.stderr to stdout/stderr
+        # make CalledProcessError contain stdout/stderr content
         sStdout = b''
-        c = proc.stdout.read(1)
-        while c != b'':
-            sStdout += c
-            sys.stdout.buffer.write(c)
-            sys.stdout.flush()
-            c = proc.stdout.read(1)
+        with pselector() as selector:
+            os.set_blocking(proc.stdout.fileno(), False)
+            selector.register(proc.stdout, selectors.EVENT_READ)
+            while selector.get_map():
+                res = selector.select(TIMEOUT)
+                for key, events in res:
+                    data = key.fileobj.read()
+                    if data == b'':
+                        selector.unregister(key.fileobj)
+                        continue
+                    sStdout += data
+                    sys.stdout.buffer.write(data)
+                    sys.stdout.flush()
 
         retcode = proc.wait()
         if retcode > 128:
             time.sleep(PARENT_WAIT)
         if retcode != 0:
             raise subprocess.CalledProcessError(retcode, proc.args, sStdout.decode(sys.stdout.encoding), "")
-
-    @staticmethod
-    def _communicateWithPty(ptyProc):
-        if hasattr(selectors, 'PollSelector'):
-            pselector = selectors.PollSelector
-        else:
-            pselector = selectors.SelectSelector
-
-        # redirect proc.stdout/proc.stderr to stdout/stderr
-        # make CalledProcessError contain stdout/stderr content
-        sStdout = ""
-        with pselector() as selector:
-            selector.register(ptyProc, selectors.EVENT_READ)
-            while selector.get_map():
-                res = selector.select(TIMEOUT)
-                for key, events in res:
-                    try:
-                        data = key.fileobj.read(1)
-                    except EOFError:
-                        selector.unregister(key.fileobj)
-                        continue
-                    sStdout += data
-                    sys.stdout.write(data)
-                    sys.stdout.flush()
-
-        ptyProc.wait()
-        if ptyProc.signalstatus is not None:
-            time.sleep(PARENT_WAIT)
-        if ptyProc.exitstatus:
-            raise subprocess.CalledProcessError(ptyProc.exitstatus, ptyProc.argv, sStdout, "")
-
-    @staticmethod
-    def _communicateWithPtyStuckCheck(ptyProc, bQuiet):
-        if hasattr(selectors, 'PollSelector'):
-            pselector = selectors.PollSelector
-        else:
-            pselector = selectors.SelectSelector
-
-        # redirect proc.stdout/proc.stderr to stdout/stderr
-        # make CalledProcessError contain stdout/stderr content
-        # terminate the process and raise exception if they stuck
-        sStdout = ""
-        bStuck = False
-        with pselector() as selector:
-            selector.register(ptyProc, selectors.EVENT_READ)
-            while selector.get_map():
-                res = selector.select(TIMEOUT)
-                if res == []:
-                    bStuck = True
-                    if not bQuiet:
-                        sys.stderr.write("Process stuck for %d second(s), terminated.\n" % (TIMEOUT))
-                    ptyProc.terminate()
-                    break
-                for key, events in res:
-                    try:
-                        data = key.fileobj.read(1)
-                    except EOFError:
-                        selector.unregister(key.fileobj)
-                        continue
-                    sStdout += data
-                    sys.stdout.write(data)
-                    sys.stdout.flush()
-
-        ptyProc.wait()
-        if ptyProc.signalstatus is not None:
-            time.sleep(PARENT_WAIT)
-        if bStuck:
-            raise ProcessStuckError(ptyProc.args, TIMEOUT)
-        if ptyProc.exitstatus:
-            raise subprocess.CalledProcessError(ptyProc.exitstatus, ptyProc.argv, sStdout, "")
 
     @staticmethod
     def domainNameIsPrivate(domainName):
